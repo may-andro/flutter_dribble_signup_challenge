0557a2639573942ce6dae3969b66d610bd0f3bca
mayankrai <mayank.rai@mutualmobile.com>
2019-02-01 15:31:22 +0530

    removed unwanted code

--- lib/component/rounded_button.dart
+++ lib/component/rounded_button.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_challenge_login/utility/gradient.dart';
 
 class RoundedButton extends StatelessWidget {
   final Function onPressed;
